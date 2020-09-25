1f5000e260ca37ca5c5e2ee926209fabedf6512a
jasoncyao <chihsuanyiu@gmail.com>
2020-09-25 14:51:59 -0700

    add pwd to see which directory I'm in in circleci

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,3 +2,4 @@
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 unzip packages/regression_model/regression_model/datasets/*.zip
+pwd
