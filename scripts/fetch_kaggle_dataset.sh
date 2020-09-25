8c112bf574dd6d8c22c6530fb4c3d2935502a15a
jasoncyao <chihsuanyiu@gmail.com>
2020-09-25 15:11:57 -0700

    add ls to see which if files are there

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -2,4 +2,5 @@
 
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
 unzip packages/regression_model/regression_model/datasets/*.zip
+ls packages/regression_model/regression_model/datasets
 ls /home/circleci/project/packages/regression_model/regression_model/datasets
