78eae297c811b394d72f62745d0c9457d4e485f6
jasoncyao <chihsuanyiu@gmail.com>
2020-09-25 15:29:43 -0700

    modify kaggle download statements to download individual files instead as zip file

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-unzip packages/regression_model/regression_model/datasets/*.zip
+kaggle competitions download -c house-prices-advanced-regression-techniques -f test.csv -p packages/regression_model/regression_model/datasets/
+kaggle competitions download -c house-prices-advanced-regression-techniques -f train.csv -p packages/regression_model/regression_model/datasets/
 ls packages/regression_model/regression_model/datasets
 ls /home/circleci/project/packages/regression_model/regression_model/datasets
